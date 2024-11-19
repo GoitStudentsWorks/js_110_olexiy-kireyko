610154e973967c92333b2024baa7a8507990941e
olexiy-kireyko <a.kireyko@gmail.com>
2024-11-20 00:29:41 +0200

    alex

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './js/faq';
+import './js/header';
+import './js/my-projects';
+import './js/reviews';
+import './js/work-together';
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
