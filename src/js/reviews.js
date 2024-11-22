9957166e5ad1653d69f38725db82a9a0e943421a
ptrnk0 <vlad.ptrnko@gmail.com>
2024-11-22 16:07:13 +0200

    Add function for create markup and get reviews

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,13 +1,39 @@
 import axios from 'axios';
+import Swiper from 'swiper';
 
 const BASE_URL = 'https://portfolio-js.b.goit.study';
 const END_POINT = '/api/reviews';
+const slideList = document.querySelector('.swiper-wrapper');
+const swiper = new Swiper('.swiper', {
+  speed: 400,
+  spaceBetween: 100,
+});
 
 async function getReviews() {
   const { data } = await axios(BASE_URL + END_POINT);
-  console.log(data);
-
   return data;
 }
 
-await getReviews();
+function markupReviews(arr) {
+  return arr
+    .map(
+      ({ author, avatar_url, review }) =>
+        `<div class="swiper-slide">
+          <blockquote class="reviews__quote">
+            <p class="reviews__text">${review}</p>
+          </blockquote>
+
+          <div class="reviews__info">
+            <div class="reviews__wrapper__img">
+              <img src="${avatar_url}" alt="client" class="reviews__img">
+            </div>
+            <h3 class="reviews__name">${author}</h3>
+          </div>
+        </div>`
+    )
+    .join('');
+}
+
+getReviews().then(data => {
+  slideList.innerHTML = markupReviews(data);
+});
