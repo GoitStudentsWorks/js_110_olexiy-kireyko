508501dd8d91bb166c15d42d65832740579f0fba
ptrnk0 <vlad.ptrnko@gmail.com>
2024-11-22 17:04:02 +0200

    Change markup

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -3,12 +3,25 @@ import Swiper from 'swiper';
 
 const BASE_URL = 'https://portfolio-js.b.goit.study';
 const END_POINT = '/api/reviews';
-const slideList = document.querySelector('.swiper-wrapper');
+const swiperList = document.querySelector('.swiper-wrapper');
 const btnPrev = document.querySelector('.swiper__button-prev');
 const btnNext = document.querySelector('.swiper__button-next');
 const swiper = new Swiper('.swiper', {
   speed: 400,
-  spaceBetween: 100,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 1,
+    },
+    720: {
+      slidesPerView: 1,
+      spaceBetween: 1,
+    },
+    1280: {
+      slidesPerView: 2,
+      spaceBetween: 32,
+    },
+  },
 });
 
 async function getReviews() {
@@ -20,25 +33,29 @@ function markupReviews(arr) {
   return arr
     .map(
       ({ author, avatar_url, review }) =>
-        `<div class="swiper-slide">
-          <blockquote class="reviews__quote">
-            <p class="reviews__text">${review}</p>
-          </blockquote>
-
+        `<li class="swiper-slide">
+            <blockquote class="reviews__quote">
+              <p class="reviews__text">${review}</p>
+            </blockquote>
           <div class="reviews__info">
             <div class="reviews__wrapper__img">
               <img src="${avatar_url}" alt="client" class="reviews__img">
             </div>
             <h3 class="reviews__name">${author}</h3>
           </div>
-        </div>`
+        </li>`
     )
     .join('');
 }
 
-getReviews().then(data => {
-  slideList.innerHTML = markupReviews(data);
-});
+getReviews()
+  .then(data => {
+    swiperList.innerHTML = markupReviews(data);
+  })
+  .catch(Error => {
+    alert('Reviews error' + Error);
+    swiperList.innerHTML = `<p>Not found</p>`;
+  });
 
 btnNext.addEventListener('click', event => swiper.slideNext());
 
