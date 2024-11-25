19e50065cb18c006e8cd2659e70419587a2b3a30
vitalysementsov <v.sementsov@ratkus.com.ua>
2024-11-25 17:11:14 +0200

    fix faq

--- src/js/faq.js
+++ src/js/faq.js
@@ -3,7 +3,7 @@ import 'accordion-js/dist/accordion.min.css';
 
 new Accordion('.faq__accordion-container', {
   openOnInit: [0],
-  collapse: false,
+  collapse: true,
   elementClass: 'faq__acc-item',
   triggerClass: 'faq__acc-trigger',
   panelClass: 'faq__acc-panel',
